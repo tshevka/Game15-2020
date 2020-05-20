fae142757287f35cbeb6cbcc432a24026caca2f4
tshevka <katekuzmenko.kk13@gmail.com>
2020-05-20 15:45:45 +0300

    Добавьте файлы проекта.

---
+++ Game_15/FormGame15.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_15
+{
+    public partial class FormGame15 : Form
+    {
+        Game game;
+
+        public FormGame15()
+        {
+            InitializeComponent();
+            game = new Game(4);//size = 4
+        }
+
+        private void button15_Click(object sender, EventArgs e)//show button's position
+        {
+            int position = Convert.ToInt32(((Button)sender).Tag);
+            game.Switch(position);//міняю місцями комірки
+            refresh();//оновлення грального поля
+            if (game.check_positions())
+            {
+                MessageBox.Show("Ви перемогли! Вітання!");
+                start_game();
+            }
+
+        }
+
+      
+        private Button button(int position)//writes button's position/number
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+
+        private void FormGame15_Load(object sender, EventArgs e)
+        {
+            start_game();
+        }
+        private void menu_start_Click(object sender, EventArgs e)
+        {
+
+            start_game();
+        }
+
+        private void start_game()
+        {
+            game.start();
+            for (int j = 0; j < 100; j++)//mix array 
+            {
+                game.Switch_random();
+            }
+            refresh();
+        }
+
+        private void refresh()//оновлює вміст кнопок 
+        {
+            for (int position = 0; position < 16; position++)
+            {
+                int number = game.get_position(position);
+
+                button(position).Text = number.ToString();// пише номер позиції кнопки 
+                button(position).Visible = (number > 0);
+            }
+        }
+
+        private void проПрограмуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Info info = new Info();
+            info.ShowDialog();
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+      
+    }
+}
---
+++ Game_15/Game.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_15
+{
+    class Game
+    {
+        int size;
+        int[,] map;
+        int space_x, space_y;
+        static Random rnd = new Random(); // static, щоб не повертав однакові числа
+        public Game(int size)
+        {
+            if (size < 2) size = 2;
+            if (size > 5) size = 5;
+            this.size = size;
+            map = new int[size, size];
+
+        }
+
+        public void start()
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    map[x, y] = coords_to_position(x, y) + 1;
+
+                }
+                space_x = size - 1;
+                space_y = size - 1;
+
+                map[space_x, space_y] = 0;//the latest button==0
+            }
+        }
+
+
+        public void Switch(int position)//switch button's position 
+        {
+            int x, y;
+            position_to_coords(position, out x, out y);// отримую координату в масиві
+            if (Math.Abs(space_x - x) + Math.Abs(space_y - y) != 1) return;//дозволяє переміщувати комірки лише на сусідню позицію
+            else
+            {
+                map[space_x, space_y] = map[x, y];
+                map[x, y] = 0;
+                space_x = x;
+                space_y = y;
+            }
+        }
+
+    
+        public void Switch_random()//mix array 
+        {
+            // Switch(rnd.Next(0,size* size)); - неоптимізоване рішення задачі
+            int step = rnd.Next(0, 4);//максимально можлива к-сть ходів в грі ==4, більш оптимізований варіант 
+            int x = space_x;
+            int y = space_y;
+            switch (step)
+            {
+                case 0: x--; break;// go left 
+                case 1: x++; break;//go right
+                case 2: y--; break;// go up
+                case 3: y++; break;// go down
+            }
+            Switch(coords_to_position(x, y));// викликаю функцію, передаю координати 
+        }
+
+     
+
+        public int get_position(int position)// returns button's position 
+        {
+            int x, y;
+            position_to_coords(position, out x, out y);
+            if (x < 0 || x >= size) return 0;//перевірка, щоб уникнути переповнення масива 
+            if (y < 0 || y >= size) return 0;//перевірка, щоб уникнути переповнення масива 
+
+            return map[x, y];//check it 
+        }
+
+        public bool check_positions()//перевірка результату гри 
+        {
+            if (!(space_x == size - 1 && space_y == size - 1))
+                return false;//пуста клітинка повинна бути в правому нижньому кутку
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (!(x == size - 1 && y == size - 1))//якщо це не остання комірка, то перевіряємо умову нижче 
+                        if (map[x, y] != coords_to_position(x, y) + 1)
+                            return false;
+                }
+            }
+            return true;
+        }
+
+        private int coords_to_position(int x, int y)
+        {
+            if (x < 0) x = 0;//перевірка чи координати не виходять за рамки масива 
+            if (x > size - 1) x = size - 1;
+            if (y < 0) y = 0;
+            if (y > size - 1) y = size - 1;
+            return x * size + y; // знаходить номер позиції кнопки
+        }
+
+        private void position_to_coords(int position, out int x, out int y)//обернена функція до coords_to_position
+        {
+            if (position < 0) position = 0;//перевірка на вихід за рамки 
+            if (position > size * size - 1) position = size * size - 1;
+            x = position / size;//знаходить координату х
+            y = position % size;//знаходить координату х
+        }
+
+       
+    }
+}
